ddcf7fb286e984c5ab92676f49e0235ae21f65d0
Sebastian Pichler <s.pichler100795@gmail.com>
2021-02-15 13:58:11 +0100

    add exercise

--- src/src/main.java
+++ src/src/main.java
@@ -1,14 +1,12 @@
 public class main {
     public static void main(String[] args) {
 
-        int zahl = 1;
-
         for (int j = 1; j <= 10; j++) {
 
             for (int i = 1; i <= 10; i++) {
-                System.out.printf("%d * %d = %d \n", i, zahl, i * zahl);
+                System.out.printf("%d * %d = %d \n", i, j, i * j);
             }
-            zahl++;
+            j++;
         }
     }
 }
